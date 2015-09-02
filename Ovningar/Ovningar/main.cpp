a20a74b60f1a5e1da2f5deef565ce5859d3feb62
randoro_mah <anton.arnoldsson@hotmail.com>
2015-09-02 12:48:48 +0200

    pointers övning

--- Ovningar/Ovningar/main.cpp
+++ Ovningar/Ovningar/main.cpp
@@ -1,9 +1,38 @@
 #include <iostream>
 using namespace std;
 
+
+
+class Pointers {
+public:
+	Pointers() {
+		cout << "sets int value1 to 50" << endl;
+		int value1 = 50;
+		cout << "sets int value2 to 25" << endl;
+		int value2 = 25;
+		cout << "creates pointer ptr and sets adress to value1's address" << endl;
+		int* ptr = &value1;
+
+		cout << "value1:" << value1 << endl;
+		cout << "value2:" << value2 << endl;
+		cout << "ptr:" << *ptr << endl;
+
+		cout << "changes ptr to adress of value2" << endl;
+		ptr = &value2;
+
+		cout << "value1:" << value1 << endl;
+		cout << "value2:" << value2 << endl;
+		cout << "ptr:" << *ptr << endl;
+	}
+};
+
 int main() 
 {
+	Pointers();
 
+	_sleep(-1);
 	return 0;
 
 }
+
+
